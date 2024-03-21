0c11feaf86dd2e2688522382900c67ed7e310aba
mels <chanatan.melmel@gmail.com>
2024-03-21 20:36:34 +0700

    feat: added search suggestion

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,4 +1,5 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState, useRef } from "react";
+
 import { IoSearch } from "react-icons/io5";
 
 const Searchbar = ({
@@ -8,6 +9,22 @@ const Searchbar = ({
   setFilteredItems,
   allItems,
 }) => {
+  const wrapperRef = useRef(null);
+  const [showingOption, setShowingOption] = useState(false);
+  const toggleShowingOption = () => {
+    setShowingOption((prev) => !prev);
+  };
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return document.addEventListener("mousedown", handleClickOutside);
+  });
+
+  const handleClickOutside = () => {
+    if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+      setShowingOption(false);
+    }
+  };
+
   const handleSearch = (e) => {
     console.log(filteredItems);
     setSearchTerm(e.target.value);
@@ -19,8 +36,8 @@ const Searchbar = ({
   };
 
   return (
-    <>
-      <span className=" px-3 py-2 rounded-full flex items-center gap-2 glassmorphism !border-gray-500 ">
+    <div onClick={() => setShowingOption(true)} ref={wrapperRef}>
+      <span className=" px-3 py-2 rounded-full flex items-center gap-2 bg-white !border-gray-500 text-black">
         <IoSearch className="fill-gray-500" />
         <input
           type="text"
@@ -30,12 +47,23 @@ const Searchbar = ({
           className="placeholder:text-gray-500 placeholder:select-none focus:outline-none bg-transparent paragraph text-center w-full pr-2"
         />
       </span>
-      <div>
-        {filteredItems.map((item, index) => {
-          return <li> {item} </li>;
-        })}
-      </div>
-    </>
+      {showingOption && (
+        <div className="p-2 bg-white text-black rounded-xl flex flex-col mt-2">
+          {filteredItems.length != 0 ? (
+            filteredItems.map((item, index) => {
+              return (
+                <a className="p-2 hover:bg-gray-400 flex-1 rounded-lg">
+                  {" "}
+                  {item}{" "}
+                </a>
+              );
+            })
+          ) : (
+            <p className="text-gray-400">no search result</p>
+          )}
+        </div>
+      )}
+    </div>
   );
 };
 
