23a5dc6dfdec2da96d8385fc0e5b974f1503bd51
melchanatan <chanatan@outlook.com>
2024-03-23 22:53:30 +0700

    feat: change MyWebcam facingMode to enviroment

--- src/components/MyWebcam.js
+++ src/components/MyWebcam.js
@@ -6,6 +6,12 @@ import { IoCameraOutline } from "react-icons/io5";
 const MyWebcam = ({ params, setImage }) => {
   const [cameraFakeCapturing, setCameraFakeCapturing] = useState(false);
 
+  const videoConstraints = {
+    width: 720,
+    height: 1280,
+    facingMode: "environment",
+  };
+
   const handleCameraCapturing = () => {
     setCameraFakeCapturing(true);
 
@@ -13,13 +19,18 @@ const MyWebcam = ({ params, setImage }) => {
       setCameraFakeCapturing(false);
     }, 100);
   };
+
   return (
     <>
       <div className="w-[100%] h-[100%] relative flex justify-center items-center">
         {cameraFakeCapturing && (
           <div className="w-full h-full bg-gray-200 absolute top-0 left-0"></div>
         )}
-        <Webcam audio={false} screenshotFormat="image/jpeg">
+        <Webcam
+          audio={false}
+          screenshotFormat="image/jpeg"
+          videoConstraints={videoConstraints}
+        >
           {({ getScreenshot }) => (
             <button
               className="bg-white p-5 rounded-full absolute left-[50%] translate-x-[-50%] bottom-[-48px] active:scale-90 transition-all active:brightness-75"
