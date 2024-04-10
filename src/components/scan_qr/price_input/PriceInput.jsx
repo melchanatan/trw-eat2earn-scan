fe0755dd9d3b5c185d8e1c6faf00ff604a408626
melchanatan <chanatan@outlook.com>
2024-04-10 14:42:08 +0700

    feat: handle usd format on PriceInput

--- src/components/scan_qr/price_input/PriceInput.jsx
+++ src/components/scan_qr/price_input/PriceInput.jsx
@@ -4,6 +4,55 @@ import { FormContext } from "../../../utils/scan_qr/FormProvider";
 const PriceInput = () => {
   const { amount, setAmount } = useContext(FormContext);
 
+  const handleChange = (e) => {
+    var buffer = e.target.value;
+    // handle us dollar formatting
+
+    // remove leading zero
+    if (buffer.startsWith("0")) {
+      buffer = buffer.substring(1);
+    }
+
+    // limit to 2 decimal places
+    if (buffer.includes(".")) {
+      const parts = buffer.split(".");
+      if (parts[1].length > 2) {
+        buffer = parts[0] + "." + parts[1].substring(0, 2);
+      }
+    }
+
+    setAmount(buffer);
+  };
+
+  const handleOnBlur = (e) => {
+    var buffer = e.target.value;
+
+    // handle us dollar formatting
+    if (buffer === "" || buffer === "0" || buffer === ".00") {
+      buffer = "0.00";
+    }
+
+    // set .00 on the back of the string
+    if (!buffer.includes(".")) {
+      buffer = buffer + ".00";
+    }
+
+    // set 0 on the front of the string
+    if (buffer.startsWith(".")) {
+      buffer = "0" + buffer;
+    }
+
+    // add leading zero if there is only one decimal placement
+    if (buffer.includes(".")) {
+      const parts = buffer.split(".");
+      if (parts[1].length < 2) {
+        buffer = parts[0] + "." + parts[1] + "0";
+      }
+    }
+
+    setAmount(buffer);
+  };
+
   return (
     <div className="input !px-6">
       <input
@@ -13,7 +62,8 @@ const PriceInput = () => {
         step="any"
         className="bg-transparent text-lg text-black outline-0 font-futura placeholder:text-gray-300 text-center w-full"
         placeholder="0.00"
-        onChange={(e) => setAmount(e.target.value)}
+        onChange={handleChange}
+        onBlur={handleOnBlur}
       />
 
       <div className="flex items-center">
