bf164472d5ba9e9cbdb74cf283ad47d6fc9849c6
SecretKr <kraiwit.wit@gmail.com>
2024-04-10 23:10:09 +0700

    feat: update user info in loyalty page

--- src/components/loyalty/HistoryGridBox.jsx
+++ src/components/loyalty/HistoryGridBox.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { TiStarFullOutline } from "react-icons/ti";
 
-const HistoryGridBox = () => {
+const HistoryGridBox = ({point}) => {
   return (
     <div className="bg-gradient-primary grid-box justify-center items-center flex-col text-background">
       <span className="p-1 bg-white/20 inline-block rounded-full mb-3">
         <TiStarFullOutline className="fill-secondary w-[32px] h-[32px] " />
       </span>
-      <h2 className="font-medium">12,000</h2>
+      <h2 className="font-medium">{point}</h2>
       <p className="">points</p>
     </div>
   );
