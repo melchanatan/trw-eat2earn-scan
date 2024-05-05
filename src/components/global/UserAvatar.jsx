6230d104609ae78b0ae0c1c3f5c921eef1466017
melchanatan <chanatan@outlook.com>
2024-05-05 21:29:28 +0700

    feat: added signout button to UserAvatar

--- src/components/global/UserAvatar.jsx
+++ src/components/global/UserAvatar.jsx
@@ -1,19 +1,33 @@
-import React, { useContext } from "react";
+"use client"
+import React, { useContext, useState } from "react";
 import Image from "next/image";
 import { UserInfoContext } from "../../utils/UserInfoProvider";
+import { LuLogOut } from "react-icons/lu";
 
 const UserAvatar = ({ className = "", label = "Welcome back," }) => {
   const { name } = useContext(UserInfoContext);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleIsOpen = () => {
+    setIsOpen(prev => !prev)
+  }
 
   return (
-    <div className={`flex font-avant gap-3 pl-0 ${className}`}>
-      <Image
-        src={"/assets/avatar-placeholder.png"}
-        width={48}
-        height={48}
-        alt={"usa-flag-avatar"}
-        className="rounded-[14px]"
-      />
+    <div className={`flex font-avant gap-3 pl-0 relative ${className}`} onClick={toggleIsOpen}>
+      <button  className="">
+        <Image
+          src={"/assets/avatar-placeholder.png"}
+          width={48}
+          height={48}
+          alt={"usa-flag-avatar"}
+          className="rounded-[14px]"
+        />
+      </button>
+       {
+        isOpen && ( 
+          <Overlay />
+         )
+      } 
       <div className="flex flex-col justify-center items-start">
         <label>{label}</label>
         <h2>{!name ? <div className="w-[10ch] h-6 loading"></div> : name}</h2>
@@ -21,5 +35,17 @@ const UserAvatar = ({ className = "", label = "Welcome back," }) => {
     </div>
   );
 };
+
+const Overlay = () => {
+  return (
+      <>
+          <div
+              className="absolute bottom-[-42px] left-0 bg-white rounded-md py-2 px-6 divide-y shadow-md"
+          >
+             <button onClick={() => {}} className="font-bold flex gap-2 items-center"> <LuLogOut /> Logout</button>
+          </div >
+      </>
+  )
+}
 
 export default UserAvatar;
