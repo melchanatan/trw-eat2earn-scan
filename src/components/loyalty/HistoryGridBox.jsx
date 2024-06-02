fdedf5ed6a8e71fd6e6e099a809ba50ce181758c
melchanatan <chanatan@outlook.com>
2024-06-02 20:27:26 +0700

    feat: change 'GO YOU' to 'GO YOU!' in HistoryGridBox

--- src/components/loyalty/HistoryGridBox.jsx
+++ src/components/loyalty/HistoryGridBox.jsx
@@ -3,11 +3,11 @@ import { FaArrowRightLong } from "react-icons/fa6";
 
 const HistoryGridBox = ({ onClick, historySum }) => {
   return (
-    <div className="col-span-full bg-gradient-primary-lighter grid-box grid grid-cols-2 !p-6 !pb-0">
+    <div className="col-span-full bg-gradient-primary-lighter grid-box grid grid-cols-2 !p-4 !pb-0">
       <div className="flex flex-col items-start justify-between translate-x-[10px]">
         <h2 className="!text-xl !leading-6 !font-bold ">
           {" "}
-          Wow, <br /> you've piled up  {historySum} points this week!
+          Wow, <br /> you've piled up {historySum} points this week!
         </h2>
         <button
           href=""
@@ -17,7 +17,7 @@ const HistoryGridBox = ({ onClick, historySum }) => {
           Check <FaArrowRightLong className="group-hover:animate-slide" />
         </button>
       </div>
-      <img src="/assets/go-you-tickets.svg" alt="tickets image" className="translate-y-[8px] translate-x-[10px]" />
+      <img src="/assets/go-you-tickets.svg" alt="tickets image" className="translate-y-[9px] translate-x-[10px]" />
     </div>
   );
 };
