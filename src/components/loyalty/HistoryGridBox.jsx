cde1695209376f9cbabd4743ce00c6539b12bffc
melchanatan <chanatan@outlook.com>
2024-06-24 22:54:14 +0700

    feat: change HistoryGridBox styling

--- src/components/loyalty/HistoryGridBox.jsx
+++ src/components/loyalty/HistoryGridBox.jsx
@@ -1,23 +1,31 @@
 import React from "react";
 import { FaArrowRightLong } from "react-icons/fa6";
+import { RiQrScanLine } from "react-icons/ri";
 
 const HistoryGridBox = ({ onClick, historySum }) => {
   return (
-    <div onClick={onClick} className="col-span-full bg-gradient-primary-lighter grid-box grid grid-cols-2 !p-4 !pb-0 cursor-pointer">
-      <div className="flex flex-col items-start justify-between translate-x-[10px]">
-        <h2 className="!text-xl !leading-6 !font-bold">
+    <div
+      onClick={onClick}
+      className="col-span-full bg-gradient-primary-lighter grid-box grid grid-cols-2 !p-4 !pb-0 cursor-pointer h-[220px] relative"
+    >
+      <div className="flex flex-col items-start justify-between translate-x-[10px] z-10">
+        <h2 className="!text-2xl !leading-8 !font-bold w-[13ch] pt-6">
           {" "}
-          Wow, <br /> you've piled up {historySum} points this week!
+          Wow, <br /> you've piled up {historySum} points
         </h2>
         <button
           href=""
-          className="flex gap-2 items-center justify-center bg-gradient-primary text-white rounded-full px-5 py-1 mb-5 group"
+          className="text-md flex gap-2 items-center justify-center bg-gradient-primary text-white rounded-lg px-3 py-2 mb-5 group"
           onClick={onClick}
         >
-          Scan <FaArrowRightLong className="group-hover:animate-slide" />
+          Scan QR Code <RiQrScanLine className="w-[24px] h-[24px]" />
         </button>
       </div>
-      <img src="/assets/go-you-tickets.svg" alt="tickets image" className="translate-y-[9px] translate-x-[10px]" />
+      <img
+        src="/assets/go-you-tickets.svg"
+        alt="tickets image"
+        className="absolute bottom-[-9px] right-[4px] w-[60%]"
+      />
     </div>
   );
 };
