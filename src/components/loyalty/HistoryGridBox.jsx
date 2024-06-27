8ad8dcb71e3f402e613c83251325e7fc11679863
melchanatan <chanatan@outlook.com>
2024-06-27 17:08:57 +0700

    feat: ui change on HistoryGridBox

--- src/components/loyalty/HistoryGridBox.jsx
+++ src/components/loyalty/HistoryGridBox.jsx
@@ -8,11 +8,12 @@ const HistoryGridBox = ({ onClick, historySum }) => {
       onClick={onClick}
       className="col-span-full bg-gradient-primary-lighter grid-box grid grid-cols-2 !p-4 !pb-0 cursor-pointer h-[220px] relative"
     >
-      <div className="flex flex-col items-start justify-between translate-x-[10px] z-10">
-        <h2 className="!text-2xl !leading-8 !font-bold w-[13ch] pt-6">
-          {" "}
-          Wow, <br /> you've piled up {historySum} points
-        </h2>
+      <div className="ml-auto flex flex-col items-start justify-between translate-x-[10px] z-10">
+        <tl className="text-sm w-[20ch] pt-6 list-decimal text-slate-600">
+          <li>Enter your spend amount</li>
+          <li>Upload your receipt</li>
+          <li>Scan the restaurant QR code</li>
+        </tl>
         <button
           href=""
           className="text-md flex gap-2 items-center justify-center bg-gradient-primary text-white rounded-lg px-3 py-2 mb-5 group"
@@ -22,9 +23,9 @@ const HistoryGridBox = ({ onClick, historySum }) => {
         </button>
       </div>
       <img
-        src="/assets/go-you-tickets.svg"
+        src="/assets/eat2earn-tickets.svg"
         alt="tickets image"
-        className="absolute bottom-[-9px] right-[4px] w-[60%]"
+        className="absolute bottom-[-9px] left-[4px] w-[60%]"
       />
     </div>
   );
